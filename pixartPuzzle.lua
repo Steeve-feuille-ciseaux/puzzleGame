9ea7bcae686c44c18b6a3356e76fcae50e6729c6
jeff peterson <libertybanshee1@gmail.com>
2025-02-16 15:27:43 +0100

    v0.04.0

--- pixartPuzzle.lua
+++ pixartPuzzle.lua
@@ -7,6 +7,8 @@
 -- script:  lua
 
 -- Script: Affichage de la grille uniquement
+x = 100
+endPuzzle = 0
 
 -- Définition de la carte MAP
 MAP = {
@@ -39,6 +41,27 @@ MAP.COLOR = {00,02,03,04,05,06,09,10,11}
 PIXEL = {}
 PIXEL.COLOR = {}
 
+-- Met à jour la carte lorsque le puzzle est fini
+function completePuzzle()
+    cls(0) -- Efface l'écran    -- Dessiner la carte
+    for y = 1, #MAP do
+        for x = 1, #MAP[y] do
+            local color = MAP[y][x]
+            local posX = MAP.POS_X + (x - 1) * MAP.CELL_SIZE
+            local posY = MAP.POS_Y + (y - 1) * MAP.CELL_SIZE
+
+            if color == 99 then
+                MAP[y][x] = 0 -- Remplace 99 par 0
+            else
+                -- Sinon, dessiner normalement
+                rect(posX, posY, MAP.CELL_SIZE, MAP.CELL_SIZE, color)
+            end
+        end
+    end
+    print("Finish !!!", 187,123 - 5,12)
+    print(endPuzzle .. " / " .. tostring(x), 190,130 - 5,12)
+end
+
 function TIC()
     cls(0) -- Efface l'écran
 
@@ -53,6 +76,10 @@ function TIC()
                 -- Si la valeur est 99, dessiner avec une bordure
                 rect(posX, posY, MAP.CELL_SIZE, MAP.CELL_SIZE, 8) 
                 rectb(posX, posY, MAP.CELL_SIZE, MAP.CELL_SIZE, 13)
+            elseif color == 99 and endPuzzle == x then
+                -- Si le puzzle est fini, la pixel inutile s'efface
+                rect(posX, posY, MAP.CELL_SIZE, MAP.CELL_SIZE, 0) 
+                rectb(posX, posY, MAP.CELL_SIZE, MAP.CELL_SIZE, 0)
             else
                 -- Sinon, dessiner normalement
                 rect(posX, posY, MAP.CELL_SIZE, MAP.CELL_SIZE, color)
@@ -70,7 +97,16 @@ function TIC()
 
     -- Affiche les coordonné X et Y de la souris
 	mX,mY,lb,mb,rb,scrollX,scrollY= mouse()
-	print(mX, 215,100,12)
-	print(mY, 215,110,12)
+	print(mX, 20,100,12)
+	print(mY, 20,110,12)
+
+    -- Affiche la progression
+    if endPuzzle == x then 
+        print("Finish !!!", 195,130 - 5,12)
+        completePuzzle()
+    else
+        print("Complete", 187,123 - 5,12)
+        print(endPuzzle .. " / " .. tostring(x), 195,130 - 5,12)
+    end
 
 end
