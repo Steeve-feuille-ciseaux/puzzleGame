3faf31f9e65cb135560d5e66df58dbcb042d37b8
jeff peterson <libertybanshee1@gmail.com>
2025-02-18 16:36:45 +0100

    v0.12.1

--- pixartPuzzle.lua
+++ pixartPuzzle.lua
@@ -8,7 +8,7 @@
 
 -- Script: Affichage de la grille uniquement
 x = 100
-endPuzzle = 0
+endPuzzle = false
 
 -- Dessin sur la carte 
 MAP = {
@@ -38,27 +38,26 @@ MAP.POS_X = 70
 MAP.POS_Y = 5
 MAP.COLOR = {00,02,03,04,05,06,09,10,11}
 
-
 -- Grille vierge
 GRID = {
-    {99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99},
-    {99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99},
-    {99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99},
-    {99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99},
-    {99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99},
-    {99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99},
-    {99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99},
-    {99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99},
-    {99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99},
-    {99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99},
-    {99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99},
-    {99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99},
-    {99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99},
-    {99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99},
-    {99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99},
-    {99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99},
-    {99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99},
-    {99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99,99},
+    {99,99,99,99,99,99,99,99,99,00,99,99,99,99,99,99,99,99,99},
+    {99,99,99,99,99,99,99,99,00,02,00,99,99,99,99,99,99,99,99},
+    {99,99,99,99,99,99,99,00,02,02,03,00,99,99,99,99,99,99,99},
+    {99,99,99,99,99,99,99,00,02,03,03,00,99,99,99,99,99,99,99},
+    {99,99,99,99,99,99,00,02,03,03,03,04,00,99,99,99,99,99,99},
+    {99,99,99,99,99,99,00,03,03,03,04,04,00,99,99,99,99,99,99},
+    {00,00,00,00,00,00,03,03,03,04,04,04,05,00,00,00,00,00,00},
+    {00,02,02,02,02,03,03,00,04,04,04,00,05,05,06,06,06,09,00},
+    {99,00,02,02,03,03,03,00,04,04,05,00,05,06,06,06,09,00,99},
+    {99,99,00,03,03,03,04,00,04,05,05,00,06,06,06,09,00,99,99},
+    {99,99,99,00,03,04,04,04,05,05,05,06,06,06,09,00,99,99,99},
+    {99,99,99,99,00,04,04,05,05,05,06,06,06,09,00,99,99,99,99},
+    {99,99,99,99,00,04,05,05,05,06,06,06,09,09,00,99,99,99,99},
+    {99,99,99,00,04,05,05,05,06,06,06,09,09,09,10,00,99,99,99},
+    {99,99,99,00,05,05,05,06,00,00,00,09,09,10,10,00,99,99,99},
+    {99,99,00,05,05,05,00,00,99,99,99,00,00,10,10,11,00,99,99},
+    {99,99,00,05,05,00,99,99,99,99,99,99,99,00,11,11,00,99,99},
+    {99,99,00,00,00,99,99,99,99,99,99,99,99,99,00,00,00,99,99},
 }
 
 -- Info sur la grille
@@ -66,8 +65,23 @@ GRID.CELL_SIZE = MAP.CELL_SIZE
 GRID.POS_X = MAP.POS_X
 GRID.POS_Y = MAP.POS_Y
 GRID.COLOR = MAP.COLOR
-GRID.COLOR.NB = {}
-GRID.TYPE = 0
+
+function watchEqual(t1, t2)
+    -- Vérifier si les dimensions sont les mêmes
+    if #t1 ~= #t2 then return false end  
+
+    for i = 1, #t1 do
+        if #t1[i] ~= #t2[i] then return false end  -- Vérifier la largeur de chaque ligne
+
+        for j = 1, #t1[i] do
+            if t1[i][j] ~= t2[i][j] then
+                return false  -- Dès qu'une cellule est différente, renvoyer false
+            end
+        end
+    end
+
+    return true  -- Si toutes les cases sont identiques, renvoyer true
+end
 
 -- Info sur la mini grille
 MINI = {}
@@ -95,30 +109,7 @@ end
 PIXEL = {}
 PIXEL.COLOR = {}
 
--- Met à jour la carte lorsque le puzzle est fini
-function completePuzzle()
-    -- Efface l'écran  
-    cls(0) 
-
-    -- Affiche le dessin sans grille
-    for y = 1, #MAP do
-        for x = 1, #MAP[y] do
-            local color = MAP[y][x]
-            local posX = MAP.POS_X + (x - 1) * MAP.CELL_SIZE
-            local posY = MAP.POS_Y + (y - 1) * MAP.CELL_SIZE
-
-            if color == 99 then
-                MAP[y][x] = 0 -- Remplace 99 par 0
-            else
-                -- Sinon, dessiner normalement
-                rect(posX, posY, MAP.CELL_SIZE, MAP.CELL_SIZE, color)
-            end
-        end
-    end
-    print("Finish !!!", 187,123 - 5,12)
-    print(endPuzzle .. " / " .. tostring(x), 190,130 - 5,12)
-end
-
+-- Identifie la couleur
 indexColor = 1
 selectedColor = MAP.COLOR[indexColor];  -- Stocke la couleur actuellement sélectionnée
 
@@ -143,8 +134,6 @@ crossLarg = 0.5 -- Largeur des lignes
 
 function TIC()
     cls(0) -- Efface l'écran
-
-    
 
     -- Affiche la mini-grille
     drawMiniGrid()
@@ -254,6 +243,41 @@ function TIC()
                     cellDelete = false -- Désactiver le mode suppression
                 end
             end
+        end
+    end
+
+    -- Condition de FIN    
+    -- Vérifier si MAP et GRID sont identiques avant d'appeler completePuzzle
+    
+    if watchEqual(MAP, GRID) then
+        endPuzzle = true
+        cls(0)    -- Dessiner la grille
+        for y = 1, #GRID do
+            for x = 1, #GRID[y] do
+                local color = GRID[y][x]
+                local posX = GRID.POS_X + (x - 1) * GRID.CELL_SIZE
+                local posY = GRID.POS_Y + (y - 1) * GRID.CELL_SIZE
+    
+                if color == 99 then
+                    rect(posX, posY, GRID.CELL_SIZE, GRID.CELL_SIZE, 0) 
+                else
+                    rect(posX, posY, GRID.CELL_SIZE, GRID.CELL_SIZE, color)
+                end
+    
+                -- Ajouter un point gris au centre de chaque cellule
+                -- pix(posX + GRID.CELL_SIZE // 2, posY + GRID.CELL_SIZE // 2, 13)
+            end
+        end
+        if endPuzzle then
+            local yesX = 15
+            local yesY = 76
+            local noX = 40 
+            local noY = 76
+            print("BRAVO !!!", 10, 58, 12)
+            print("One More", 10, 67, 12)
+            print("Yes", yesX, yesY, 12)
+            print("No", noX, noY, 12)
+            rect()
         end
     end
 
