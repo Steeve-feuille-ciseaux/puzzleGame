13180d75c68ebf7513b78b58effd324754dbd661
jeff peterson <libertybanshee1@gmail.com>
2025-03-26 08:12:26 +0100

    v1.17.0

--- test.lua
+++ test.lua
@@ -12,11 +12,6 @@ pagePuzzle = 1
 pageMax = 2
 indexMap = 0
 
--- DECOUPAGE DU JEU
-if swapScreen == 1 then
-    -- cls(0)
-end
-
 -- bouton droit relaché
 prev_rb = false
 
@@ -830,6 +825,7 @@ function TIC()
     if swapScreen == 0 then
         print("pix'Art Puzzle", 100, 50, 12)
         print("put any button", 100, 70, 12)
+        print("v1.17.0", 208, 130, 12) -- Version
         
         if prev_lb and not lb then
             swapScreen = 1
@@ -1182,9 +1178,7 @@ function TIC()
             colorN = 4  -- Changer la couleur de "No"
             print("No", noX, noY, colorN)
             if prev_lb and not lb then
-                -- 4 thxScreen | 0 -> titleScreen
-                swapScreen = 4
-                -- swapScreen = 0
+                swapScreen = 0
             end
         end
 
